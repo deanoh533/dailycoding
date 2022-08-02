40609c2bd232db2328b9fe9a2b81be4c1c872cf1
deanoh533 <deanoh533@gmail.com>
2022-08-02 14:05:32 +0900

    solution07

--- Solution07.java
+++ Solution07.java
@@ -1,4 +1,41 @@
 package com.codestates.coplit;
 
-public class Solution07 {//수수중
+import java.util.HashMap;
+
+public class Solution07 {
+
+
+
+    public static void main(String[] args) {
+
+        String[][] arr = {
+                {"make", "Ford"},
+                {"model", "Mustang"},
+                {"year", "1964"},
+                {"make", "Bill"}
+        };
+
+        HashMap<String, String> output = convertListToHashMap(arr);
+
+        System.out.println(output);
+    }
+
+    public static HashMap<String, String> convertListToHashMap(String[][] arr) {
+        // TODO:
+
+        HashMap<String, String> result =new HashMap<String , String>();
+
+        for (int i = 0 ; arr.length  > i ; i++)
+            if (arr[i].length == 2 && !result.containsKey(arr[i][0])){
+
+            result.put(arr[i][0], arr[i][1]);
+
+        }
+
+//        System.out.println(arr[0].length);
+//        System.out.println(arr[2].length);
+//        System.out.println(arr[3].length);
+        return result;
+    }
+
 }
